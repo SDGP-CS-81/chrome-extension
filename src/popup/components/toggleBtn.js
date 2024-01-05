ef805d90d90e3b5b9983eb78388c862f31bf756b
AfeefRazick <afeefrazickamir@gmail.com>
2024-01-05 23:27:52 +0530

    feat: implement user preferences functionality and default setting, make toggle button work with preferences

--- src/popup/components/toggleBtn.js
+++ src/popup/components/toggleBtn.js
@@ -1,9 +1,11 @@
+import { getPreferences, setPreferences } from "../../common.js";
+
 const template = document.createElement("template");
 template.innerHTML = `
     <label for="toggle-example" class="flex items-center cursor-pointer relative mb-4">
-        <input type="checkbox" id="toggle" class="sr-only peer/toggle" checked>
+        <input type="checkbox" id="toggle" class="sr-only peer/toggle ">
         <div class="bg-grey peer-checked/toggle:bg-primary h-7 w-12 rounded-full"></div>
-        <div class="absolute left-0.5 peer-checked/toggle:translate-x-[82%] top-0.5 bg-white w-6 h-6 rounded-full transition-all duration-300 ease-out"></div>
+        <div class="absolute  left-0.5 peer-checked/toggle:translate-x-[82%] top-0.5 bg-white w-6 h-6 rounded-full transition-all duration-300 ease-out"></div>
     </label>
     `;
 
@@ -12,20 +14,41 @@ class ToggleBtn extends HTMLElement {
     super();
     // this.attachShadow({ mode: "open" });
   }
-  connectedCallback() {
-    this.appendChild(template.content.cloneNode(true));
-    this.addEventListener("click", () => {
-      this.querySelector("input").checked =
-        !this.querySelector("input").checked;
-    });
+  get checked() {
+    return this.hasAttribute("checked");
+  }
+  set checked(value) {
+    this.toggleAttribute("checked", value);
   }
 
-  observedAttributes() {
+  static get observedAttributes() {
     return ["checked"];
+  }
+
+  connectedCallback() {
+    this.appendChild(template.content.cloneNode(true));
+    this.toggleID = this.getAttribute("toggle-id");
+
+    getPreferences().then((object) => {
+      const preferences = object.preferences;
+      console.log(preferences);
+
+      this.checked = preferences[this.toggleID];
+    });
+
+    this.addEventListener("click", () => {
+      this.checked = !this.checked;
+    });
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === "checked") {
+      getPreferences().then(async (object) => {
+        const preferences = object.preferences;
+        preferences[this.toggleID] = this.checked;
+        await setPreferences(preferences);
+        this.querySelector("input").checked = this.checked;
+      });
     }
   }
 }
---
+++ src/popup/popup.js
@@ -0,0 +1,10 @@
+// localStorage.setItem(
+//   "preferences",
+//   JSON.stringify({
+//     audioOnly: false,
+//   })
+// );
+
+// chrome.storage.local.set({ preferences: { audioOnly: false } }).then(() => {
+//   console.log("Value is set to " + false);
+// });
