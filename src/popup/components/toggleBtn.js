65ce4959a6ba144f1ff2c05be00e2104d00d5908
AfeefRazick <afeefrazickamir@gmail.com>
2024-01-07 18:03:45 +0530

    style: toggle button container based responsiveness

--- src/popup/components/toggleBtn.js
+++ src/popup/components/toggleBtn.js
@@ -1,13 +1,18 @@
 import { getPreferences, setPreferences } from "../../common.js";
 
-const template = document.createElement("template");
-template.innerHTML = `
-    <label for="toggle-example" class="flex items-center cursor-pointer relative mb-4">
-        <input type="checkbox" id="toggle" class="sr-only peer/toggle ">
-        <div class="bg-grey peer-checked/toggle:bg-primary h-7 w-12 rounded-full"></div>
-        <div class="absolute  left-0.5 peer-checked/toggle:translate-x-[82%] top-0.5 bg-white w-6 h-6 rounded-full transition-all duration-300 ease-out"></div>
-    </label>
-    `;
+const generateTemplate = () => {
+  const template = document.createElement("template");
+  template.innerHTML = `
+    <div class="@container/main w-full flex items-center justify-between p-[18px] @[400px]/main:p-[22px] bg-secondary_variant mb-4">
+        <label for="toggle-example" class="flex items-center cursor-pointer relative ">
+            <input type="checkbox" id="toggle" class="sr-only peer/toggle ">
+            <div class="bg-grey peer-checked/toggle:bg-primary rounded-full h-6 w-10 @[400px]/main:h-7 @[400px]/main:w-12"></div>
+            <div class="absolute left-0.5 top-0.5 peer-checked/toggle:translate-x-[82%] bg-white w-5 h-5 @[400px]/main:w-6 @[400px]/main:h-6 rounded-full transition-all duration-300 ease-out"></div>
+        </label>
+    </div>
+        `;
+  return template;
+};
 
 class ToggleBtn extends HTMLElement {
   constructor() {
@@ -26,7 +31,7 @@ class ToggleBtn extends HTMLElement {
   }
 
   connectedCallback() {
-    this.appendChild(template.content.cloneNode(true));
+    this.appendChild(generateTemplate().content.cloneNode(true));
     this.toggleID = this.getAttribute("toggle-id");
 
     getPreferences().then((object) => {
@@ -39,6 +44,8 @@ class ToggleBtn extends HTMLElement {
     this.addEventListener("click", () => {
       this.checked = !this.checked;
     });
+
+    this.style.width = "100%";
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
