93837f247cc4c605b08c7e472a5f6cf3c68e63f8
AfeefRazick <afeefrazickamir@gmail.com>
2024-01-11 00:53:34 +0530

    feat: add setquality functionality

--- src/content_script/script.js
+++ src/content_script/script.js
@@ -1,5 +1,32 @@
 (() => {
+  const setQuality = (quality) => {
+    const vidSettingsButton = document.querySelectorAll(
+      ".ytp-settings-button"
+    )[0];
+    vidSettingsButton.click();
+
+    const settingsMenuBtns = document.querySelectorAll(".ytp-menuitem-label");
+    for (const btn of settingsMenuBtns) {
+      if (btn.innerText === "Quality") {
+        btn.click();
+        break;
+      }
+    }
+
+    const targetItems = document.querySelectorAll(
+      ".ytp-quality-menu .ytp-menuitem-label"
+    );
+
+    console.log(targetItems);
+    for (const item of targetItems) {
+      console.log(item.innerText);
+      if (item.innerText === quality) {
+        item.click();
+        break;
+      }
+    }
+  };
   setTimeout(() => {
-    console.log(document.querySelectorAll(".ytp-settings-button")[0]);
-  }, 2000);
+    setQuality("720p");
+  }, 3000);
 })();
