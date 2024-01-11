e39736b36919400b999866775456e8aa679fa1a4
AfeefRazick <afeefrazickamir@gmail.com>
2024-01-11 23:19:09 +0530

    feat: add setquality functionality for multiple languages and devices

--- src/content_script/script.js
+++ src/content_script/script.js
@@ -1,32 +1,39 @@
-(() => {
+(async () => {
+  const src = chrome.runtime.getURL("../common/constants.js");
+  var constants = await import(src);
+
   const setQuality = (quality) => {
+    // click the settings button in video player
     const vidSettingsButton = document.querySelectorAll(
       ".ytp-settings-button"
     )[0];
     vidSettingsButton.click();
 
+    // click the "Quality" button in settings menu popup
     const settingsMenuBtns = document.querySelectorAll(".ytp-menuitem-label");
     for (const btn of settingsMenuBtns) {
-      if (btn.innerText === "Quality") {
+      if (constants.qualityTitles.includes(btn.innerText)) {
         btn.click();
         break;
       }
     }
 
-    const targetItems = document.querySelectorAll(
+    // get the list of available qualities
+    const availableQualities = document.querySelectorAll(
       ".ytp-quality-menu .ytp-menuitem-label"
     );
-
-    console.log(targetItems);
-    for (const item of targetItems) {
-      console.log(item.innerText);
-      if (item.innerText === quality) {
-        item.click();
+    for (const qualityElement of availableQualities) {
+      console.log(qualityElement.innerText);
+      // check if quality is in innertext ex: "1080p" in "1080p HD"
+      if (qualityElement.innerText.includes(quality)) {
+        qualityElement.click();
         break;
       }
     }
   };
+
+  // setquality must be called after page has been loaded
   setTimeout(() => {
-    setQuality("720p");
+    setQuality("1080p");
   }, 3000);
 })();
