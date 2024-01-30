ce4fa2ae02a1d454a2409ea0a7f8720508530a1d
mnurrsha <134758500+mnurrsha@users.noreply.github.com>
2024-01-30 10:02:28 +0530

    add low background feature to full page

--- src/common/features.js
+++ src/common/features.js
@@ -3,9 +3,9 @@ export const features = {
     featureName: "Audio Only Video",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
   },
-  lowBackgroundResolution: { featureName: "Low Background Resolution", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
+  lowBackgroundResolution: { featureName: "Low Background", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
   userInitiatedPlayback: { featureName: "User-initiated Playback", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-  thumbnailHoverBlocking: { featureName: "Thumbnail Hover Video Blocking", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
+  thumbnailHoverBlocking: { featureName: "Block Thumbnail Hover", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
   blockThumbnailLoad: { featureName: "Block Thumbnail Load", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
   blockHomePageLoad: { featureName: "Block Home Page Loading", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
 };
