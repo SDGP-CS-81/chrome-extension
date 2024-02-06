ecea9121b3f25b3135f8fe5b24c9bec7fa08e588
AbrarSL <abrarsl2002@gmail.com>
2024-02-07 00:20:19 +0530

    feat: Add category conditions

--- src/common/constants.js
+++ src/common/constants.js
@@ -35,66 +35,137 @@ export const categories = {
     decscImg: "../assets/userface_categories/music.png",
     desc: "Music includes music videos and live performances",
     keyWords: ["music", "song", "lyrics"],
+    selectionConditions: {
+      backendCategories: [],
+      analysisScores: (detailScore, diffScore) => false,
+      keywordThreshold: 1,
+    },
   },
   podcast: {
     categoryName: "Podcast",
     decscImg: "../assets/userface_categories/podcast.png",
     desc: "Podcast is about all podcast content including interviews and panel discussions",
     keywords: ["podcast", "conversation", "interview", "Episode"],
+    selectionConditions: {
+      backendCategories: ["person"],
+      analysisScores: (detailScore, diffScore) => diffScore <= 1000000,
+      keywordThreshold: 1,
+    },
   },
   gaming: {
     categoryName: "Gaming",
     decscImg: "../assets/userface_categories/game.png",
     desc: "Gaming focuses on in-game content, including gameplays and walkthroughs",
     keywords: ["gameplay", "walkthrough", "PC", "PS", "game", "live"],
+    selectionConditions: {
+      backendCategories: ["person"],
+      analysisScores: (detailScore, diffScore) =>
+        detailScore >= 1200 && diffScore >= 1800000,
+      keywordThreshold: 1,
+    },
   },
   news: {
     categoryName: "News",
     decscImg: "../assets/userface_categories/news.png",
     desc: "News focuses on news headlines and videos that include worldwide updates",
     keywords: ["news", "update", "breaking", "headlines", "politics", "world"],
+    selectionConditions: {
+      backendCategories: ["news"],
+      analysisScores: (detailScore, diffScore) => detailScore >= 1000,
+      keywordThreshold: 1,
+    },
   },
   coding: {
     categoryName: "Coding",
     decscImg: "../assets/userface_categories/coding.png",
     desc: "Coding includes programming and coding content, such as coding tutorials",
     keywords: ["python", "programming", "tutorial", "software", "develop"],
+    selectionConditions: {
+      backendCategories: ["textHeavy"],
+      analysisScores: (detailScore, diffScore) =>
+        detailScore >= 1000 && diffScore <= 1600000,
+      keywordThreshold: 1,
+    },
   },
   sports: {
     categoryName: "Sports",
     decscImg: "../assets/userface_categories/sports.png",
     desc: "Sports is for all sports-related content, such as match highlights and in-game moments",
     keywords: ["football", "score", "highlights", "match", "vs"],
+    selectionConditions: {
+      backendCategories: ["sports"],
+      analysisScores: (detailScore, diffScore) =>
+        detailScore >= 1200 && diffScore >= 1200000,
+      keywordThreshold: 1,
+    },
   },
   graphics: {
     categoryName: "Graphics",
     decscImg: "../assets/userface_categories/graphics.png",
     desc: "Graphics covers animations and a variety of visually creative videos",
     keywords: ["design", "animation", "art", "digital", "creative"],
+    selectionConditions: {
+      backendCategories: ["lowGraphics"],
+      analysisScores: (detailScore, diffScore) =>
+        detailScore <= 1000 && diffScore <= 1200000,
+      keywordThreshold: 1,
+    },
   },
   food: {
     categoryName: "Food",
     decscImg: "../assets/userface_categories/food.png",
     desc: "Food includes cooking tutorials, muckbangs and food reviews",
     keywords: ["cook", "ingredients", "baking", "muckbang", "food"],
+    selectionConditions: {
+      backendCategories: ["food"],
+      analysisScores: (detailScore, diffScore) => false,
+      keywordThreshold: 1,
+    },
   },
   nature: {
     categoryName: "Nature",
     decscImg: "../assets/userface_categories/nature.png",
     desc: "Nature is for wildlife documentaries, scenic travel vlogs, and nature exploration videos",
     keywords: ["wild", "animals", "survival", "Scenic", "nature", "ocean"],
+    selectionConditions: {
+      backendCategories: ["nature"],
+      analysisScores: (detailScore, diffScore) =>
+        detailScore >= 1200 && diffScore >= 1000000,
+      keywordThreshold: 1,
+    },
   },
   demo: {
     categoryName: "Demo",
     decscImg: "../assets/userface_categories/demo.png",
     desc: "Demo covers all types of demonstration videos, such as product unboxings and practical guides",
-    keywords: ["repair", "guide", "how", "instructions"],
+    keywords: [
+      "repair",
+      "guide",
+      "how",
+      "instructions",
+      "unboxing",
+      "review",
+      "showcase",
+      "demo",
+      "demonstration",
+      "diy",
+    ],
+    selectionConditions: {
+      backendCategories: [],
+      analysisScores: (detailScore, diffScore) => false,
+      keywordThreshold: 1,
+    },
   },
   defaultQuality: {
     categoryName: "Default Quality",
     decscImg: "../assets/userface_categories/default.png",
     desc: "Default quality for all video categories",
     keywords: [],
+    selectionConditions: {
+      backendCategories: [],
+      analysisScores: (detailScore, diffScore) => false,
+      keywordThreshold: 1,
+    },
   },
 };
 
