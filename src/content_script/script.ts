df317b3d501e8bdeca20d3e488635e3efb73c6b0
AbrarSL <abrarsl2002@gmail.com>
2024-03-05 21:37:28 +0530

    feat: Drop video for background tabs
    This also improves how the audio only feature works.
    While this approach is a lot cleaner than the previous one it can get a
    little janky sometimes.
    
    Closes: #10

--- src/content_script/script.ts
+++ src/content_script/script.ts
@@ -1,25 +1,123 @@
 (async () => {
   // import additional scripts
-  (window as any).constants = await import(
-    chrome.runtime.getURL("../common/constants.js")
-  );
-  (window as any).helpers = await import(
-    chrome.runtime.getURL("../common/helpers.js")
-  );
+  const helpers = await import(chrome.runtime.getURL("../common/helpers.js"));
 
-  // audio only message handler
-  chrome.runtime.onMessage.addListener((message) => {
+  // a variable to track the original video
+  // blob url and the audio url
+  let originalSrc: string = null;
+  let audioSrc: string = null;
+
+  const storeOriginalSrcUrl = () => {
     const videoElement = document.querySelector("video");
 
-    if (!videoElement) return true;
+    if (videoElement.src.includes("blob")) {
+      originalSrc = videoElement.src;
+      console.log(`Original Src: ${originalSrc}`);
+    }
+  };
 
-    videoElement.pause();
-    videoElement.src = message.url;
-    videoElement.currentTime = 0;
-    videoElement.play();
+  const setVideoUrl = (url: string) => {
+    console.log(`Video Url: ${url}`);
+    if (url) {
+      const videoElement = document.querySelector("video");
 
-    return true;
-  });
+      videoElement.pause();
+      videoElement.src = url;
+      videoElement.currentTime--;
+      videoElement.play();
+    }
+  };
+
+  const backgroundModeListener = () => {
+    if (document.visibilityState === "hidden") {
+      setVideoUrl(audioSrc);
+    } else if (document.visibilityState === "visible") {
+      setVideoUrl(originalSrc);
+    }
+  };
+
+  // respond to settings changes
+  const settingsListener = (changes: {
+    [key: string]: chrome.storage.StorageChange;
+  }) => {
+    const preferences = changes["preferences"];
+
+    if (preferences != undefined) {
+      const newValues = preferences.newValue["features"];
+      const oldValues = preferences.oldValue["features"];
+
+      if (newValues && oldValues) {
+        const audioOnlyNew = newValues["audioOnly"];
+        const audioOnlyOld = oldValues["audioOnly"];
+
+        const backgroundNew = newValues["lowBackgroundResolution"];
+        const backgroundOld = oldValues["lowBackgroundResolution"];
+
+        console.log(
+          `audioOnlyOld: ${audioOnlyOld}, audioOnlyNew: ${audioOnlyNew}, bgOld: ${backgroundOld}, bgNew: ${backgroundNew}`
+        );
+
+        // If there were no changes to any of the required prefs
+        if (audioOnlyNew == audioOnlyOld && backgroundNew == backgroundOld)
+          return;
+
+        // audioOnly takes precedence over the background mode listener
+        if (audioOnlyNew) {
+          // remove the background listener if it exists
+          // we will unconditionally remove video streams now
+          document.removeEventListener(
+            "visibilitychange",
+            backgroundModeListener
+          );
+
+          setVideoUrl(audioSrc);
+        }
+        // the second check here is to reenable background tab mode
+        // should we disable audioOnly after having it override the
+        // background tab mode
+        else if (backgroundNew || (!audioOnlyNew && backgroundOld)) {
+          document.addEventListener("visibilitychange", backgroundModeListener);
+        } else if (!backgroundNew) {
+          document.removeEventListener(
+            "visibilitychange",
+            backgroundModeListener
+          );
+
+          setVideoUrl(originalSrc);
+        }
+        // this condition should run if we disable the features
+        else {
+          setVideoUrl(originalSrc);
+        }
+      }
+    }
+  };
+
+  const audioOnlyListener = (message: { [key: string]: string }) => {
+    chrome.runtime.onMessage.removeListener(audioOnlyListener);
+
+    // store the audio url
+    audioSrc = message["url"];
+    storeOriginalSrcUrl();
+
+    helpers
+      .getPreferences()
+      .then((prefs: any) => {
+        console.log(prefs);
+
+        const audioOnly = prefs["features"]["audioOnly"];
+        const bgTab = prefs["features"]["lowBackgroundResolution"];
+
+        if (audioOnly) {
+          setVideoUrl(audioSrc);
+        } else if (bgTab) {
+          document.addEventListener("visibilitychange", backgroundModeListener);
+        }
+      })
+      .finally(() => {
+        chrome.storage.local.onChanged.addListener(settingsListener);
+      });
+  };
 
   const runOnUrlChange = async () => {
     const observerConfig = {
@@ -29,14 +127,8 @@
     };
 
     const currentVideoID = location.href.split("v=")[1].split("&")[0];
-    const videoScores = await (window as any).helpers.getVideoScores(
-      currentVideoID
-    );
-    console.log(videoScores);
-
-    const qualityToSet = await (window as any).helpers.calcOptimumQuality(
-      videoScores
-    );
+    const videoScores = await helpers.getVideoScores(currentVideoID);
+    const qualityToSet = await helpers.calcOptimumQuality(videoScores);
 
     // perhaps reassign a null varaible "qualityToSet" and check in observer
 
@@ -97,8 +189,9 @@
     if (!hasQualityBeenSet) return vidSettingsButton.click();
   };
 
-  document.addEventListener("yt-navigate-finish", () => {
+  document.addEventListener("yt-navigate-finish", async () => {
     if (location.pathname != "/watch") return;
+    chrome.runtime.onMessage.addListener(audioOnlyListener);
     runOnUrlChange();
   });
 })();
