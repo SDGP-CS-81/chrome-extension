f7b34bf4c8120a7a9370e9ddc13aa1f121bd7d21
AfeefRazick <afeefrazickamir@gmail.com>
2024-03-23 12:27:11 +0530

    chore: change api url from local host to server url

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,5 @@
-export const apiURL = "http://localhost:5000";
+export const apiURL =
+  "https://ec2-13-233-179-121.ap-south-1.compute.amazonaws.com";
 export const qualities = [144, 240, 360, 480, 720, 1080, 1440, 2160, 4320];
 export const categoriesList = [
   "Music",
